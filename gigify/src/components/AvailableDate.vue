0d588b35c0d2a801db14602649138909a170f4b4
pbchun <pbchun12@gmail.com>
2018-06-26 14:48:07 -0600

    changed button type for availble date cards

--- gigify/src/components/AvailableDate.vue
+++ gigify/src/components/AvailableDate.vue
@@ -3,19 +3,19 @@
         <div class="available-date-cards">
             <b-card title="7/04/18">
                 <p class="card-text">Date Details</p>
-                <b-button href="#" variant="primary">Request to book this date</b-button>
+                <b-button vue-router="#" variant="primary">Request to book this date</b-button>
             </b-card>
         </div>
         <div class="available-date-cards">
             <b-card title="7/10/18">
                 <p class="card-text">Date Details</p>
-                <b-button href="#" variant="primary">Request to book this date</b-button>
+                <b-button vue-router="#" variant="primary">Request to book this date</b-button>
             </b-card>
         </div>
         <div class="available-date-cards">
             <b-card title="7/20/18">
                 <p class="card-text">Date Details</p>
-                <b-button href="#" variant="primary">Request to book this date</b-button>
+                <b-button vue-router="#" variant="primary">Request to book this date</b-button>
             </b-card>
         </div>
     </section>
@@ -31,19 +31,15 @@ export default {
 <style>
     .available-dates {
         display: flex;
-        flex-direction: row;
+        flex-direction: column;
         justify-content: center;
     }
     .available-date-cards {
         border: 1px solid darkgray;
-        border-radius: 25%;
-        height: 30vh;
-        margin-left: 2vh;
-        margin-right: 2vh;
+        margin: auto;
+        margin-top: 3vh;
+        padding-bottom: 2vh;
         padding-left: 2vh;
         padding-right: 2vh;
-    }
-    b-button {
-        padding-top: 20vh;
     }
 </style>
