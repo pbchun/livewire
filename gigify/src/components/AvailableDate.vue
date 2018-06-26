2603a70f65022b5add369dafbdebf0266beff16b
pbchun <pbchun12@gmail.com>
2018-06-26 14:31:21 -0600

    added available date cards to AvailableDate.vue

--- gigify/src/components/AvailableDate.vue
+++ gigify/src/components/AvailableDate.vue
@@ -1,11 +1,24 @@
 <template>
-    <div class="date-card">
-        <header>Date Header goes here</header>
-        <main>Date Details here</main>
-        <footer>
-            <button @click.prevent="toggleDisplay">Request to book this Date</button>
-        </footer>
-    </div>
+    <section class="available-dates">
+        <div class="available-date-cards">
+            <b-card title="7/04/18">
+                <p class="card-text">Date Details</p>
+                <b-button href="#" variant="primary">Request to book this date</b-button>
+            </b-card>
+        </div>
+        <div class="available-date-cards">
+            <b-card title="7/10/18">
+                <p class="card-text">Date Details</p>
+                <b-button href="#" variant="primary">Request to book this date</b-button>
+            </b-card>
+        </div>
+        <div class="available-date-cards">
+            <b-card title="7/20/18">
+                <p class="card-text">Date Details</p>
+                <b-button href="#" variant="primary">Request to book this date</b-button>
+            </b-card>
+        </div>
+    </section>
 </template>
 
 <script>
@@ -16,4 +29,21 @@ export default {
 </script>
 
 <style>
+    .available-dates {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+    }
+    .available-date-cards {
+        border: 1px solid darkgray;
+        border-radius: 25%;
+        height: 30vh;
+        margin-left: 2vh;
+        margin-right: 2vh;
+        padding-left: 2vh;
+        padding-right: 2vh;
+    }
+    b-button {
+        padding-top: 20vh;
+    }
 </style>
