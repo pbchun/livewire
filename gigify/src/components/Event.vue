4322ababe096f5ab16e03e9f15e0df0d6f42a035
Mike Lavin <mikelavin@MacBook-Pro-5.local>
2018-06-26 15:48:19 -0600

    Coded up a card template that will be used when you call the event component

--- gigify/src/components/Event.vue
+++ gigify/src/components/Event.vue
@@ -1,7 +1,13 @@
 <template>
     <section class="event-card">
-        <h2>This is an Event-card component</h2>
-        <p>artist info/media will do here. These components will be dynamically generated from the database, and should generally look dope AF</p>
+        <div class ='cardText'>
+            <h1 class='artistName'>{{artistName}}Artist Name</h1>
+            <h2 class='dateOfEvent'>{{date}} Jan 23 2125</h2>
+            <p class='description'>{{description}} Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Iusto rem, voluptatum ipsam dolorum unde esse deleniti voluptates commodi et 
+                magni, a consequatur ea aperiam saepe. A ea exercitationem nemo consequatur.
+            </p>
+        </div>
     </section>
 </template>
 
@@ -10,5 +16,17 @@ export default {};
 </script>
 
 <style>
-</style>
+.event-card{
+    color: white;
+    width: 390px;
+    height: 325px;
+    border: 2px solid black;
+    border-radius: 20px;
+    background-image: url('https://i.pinimg.com/736x/e1/fb/cb/e1fbcb16e52307cd4b3aa43cf9e23d68--s-hair-bands-s-rock-bands.jpg');
+}
 
+.cardText{
+    padding-top: 2em;
+    background-color: rgba(59, 59, 59, 0.5)
+}
+</style>
