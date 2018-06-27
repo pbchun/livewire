d1c9184c6b230e9676c7815c39d84746e323f090
Mike Lavin <mikelavin@MacBook-Pro-5.Home>
2018-06-26 21:00:38 -0600

    Modified the look of the text box that lays over the card

--- gigify/src/components/Event.vue
+++ gigify/src/components/Event.vue
@@ -5,7 +5,7 @@
             <h2 class='dateOfEvent'>{{date}} Jan 23 2125</h2>
             <p class='description'>{{description}} Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                 Iusto rem, voluptatum ipsam dolorum unde esse deleniti voluptates commodi et 
-                magni, a consequatur ea aperiam saepe. A ea exercitationem nemo consequatur.
+                magni, a consequatur ea aperiam saepe. A ea exercitationem nemo consequatur. 
             </p>
         </div>
     </section>
@@ -22,11 +22,15 @@ export default {};
     height: 325px;
     border: 2px solid black;
     border-radius: 20px;
-    background-image: url('https://i.pinimg.com/736x/e1/fb/cb/e1fbcb16e52307cd4b3aa43cf9e23d68--s-hair-bands-s-rock-bands.jpg');
+    background-image: url('https://www.billboard.com/files/styles/900_wide/public/media/worst-album-covers-of-the-80s-thompson-twins-billboard-600x600.jpg');
 }
 
 .cardText{
     padding-top: 2em;
-    background-color: rgba(59, 59, 59, 0.5)
+    background-color: rgba(59, 59, 59, 0.7);
+    border-radius: 20px;
+    margin: 1em;
+    padding: 1em;
+    
 }
 </style>
