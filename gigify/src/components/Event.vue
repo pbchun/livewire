1e27bd28f4a1292715d8942e5f354383da9ed37a
REED BLACK <reedblack@REEDs-MacBook-Air.local>
2018-06-27 15:22:51 -0600

    fix

--- gigify/src/components/Event.vue
+++ gigify/src/components/Event.vue
@@ -5,14 +5,6 @@
             alt=""
         >
         <div class ='cardText'>
-<<<<<<< HEAD
-            <h1 class='artistName'>Artist</h1>
-            <h2 class='dateOfEvent'>Jan 23 2125</h2>
-            <p class='description'> Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Iusto rem, voluptatum ipsam dolorum unde esse deleniti voluptates commodi et 
-                magni, a consequatur ea aperiam saepe. A ea exercitationem nemo consequatur. 
-            </p>
-=======
          <div>
             <h1 class='artistName'>{{eventData.artist}}</h1>
             <h5 class='dateOfEvent'>{{eventData.date}}</h5>
@@ -20,7 +12,6 @@
          </div>
             
 
->>>>>>> 1ac1db5912d25d06eb22a8991f30fe2324777ef7
         </div>
       
     </section>
@@ -28,45 +19,43 @@
 
 <script>
 export default {
-  props: ["eventData"],
-
+  props: ["eventData"]
 };
 </script>
 
 <style>
-    .event-card {
-        color: white;
-        width: 390px;
-        height: 325px;
-        border-radius: 10px;
-        position: relative;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    .event-card img{
-        /* border-radius: 20px; */
-        flex-shrink: 0;
-        min-width: 100%;
-        min-height: 100%
-    }
-    .cardText {
-        padding-top: 2em;
-        background-color: rgba(59, 59, 59, 0.4);
-        border-radius: 6px;
-        width: 100%;
-        height: 100%;
-        position: absolute;
-        top:0px;
-        display: flex;
-        justify-content: left;
-        align-items: end;
-    }
-    .cardText div{
-        padding-left: 20px;
-    }
-    .cardText:hover{
-        background-color: rgba(59, 59, 59, 0.7);
-    }
-
+.event-card {
+  color: white;
+  width: 390px;
+  height: 325px;
+  border-radius: 10px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.event-card img {
+  /* border-radius: 20px; */
+  flex-shrink: 0;
+  min-width: 100%;
+  min-height: 100%;
+}
+.cardText {
+  padding-top: 2em;
+  background-color: rgba(59, 59, 59, 0.4);
+  border-radius: 6px;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0px;
+  display: flex;
+  justify-content: left;
+  align-items: end;
+}
+.cardText div {
+  padding-left: 20px;
+}
+.cardText:hover {
+  background-color: rgba(59, 59, 59, 0.7);
+}
 </style>
