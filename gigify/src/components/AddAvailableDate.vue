d707db93dafe60de6abcfa769771b61f571af0df
Jacob Hinkston <JacobHinkston@gmail.com>
2018-06-29 16:05:59 -0600

    Fixed form in AddAvailableDates.vue

--- gigify/src/components/AddAvailableDate.vue
+++ gigify/src/components/AddAvailableDate.vue
@@ -1,9 +1,11 @@
 ```<template>
-    <div>
+  <div id='backgroundForm'>
+    <div id='request'>
         <h1>Add an upcoming Open Date</h1>
         <form 
-                :submitAddedEvent="submitEvent" 
-                v-on:submit.prevent="submitEvent">
+                class="add-upcoming-form"
+                :submitAddedEvent="postAvailableDate" 
+                v-on:submit.prevent="postAvailableDate">
                <input 
                     v-model="event.date" 
                     class="form-control mb-2 mr-sm-2"
@@ -16,7 +18,7 @@
                     class="form-control mb-2 mr-sm-2"
                     type="text"
                     name="venue"
-                    placeholder="Venue"
+                    placeholder="Hi-Dive"
                     readonly
                />
                 <input 
@@ -28,6 +30,8 @@
                 />
             </form>
     </div>
+  </div>
+    
 </template>
 
 <script>
@@ -49,13 +53,18 @@ export default {
         },
         method: "POST",
         body: JSON.stringify(this.event)
-      });
+      })
+      .then(res => res.json())
+      .then(resJSON => console.log(resJSON))
     }
   }
 };
 </script>
 
 <style>
+.add-upcoming-form {
+
+}
 #submitButton {
   font-size: 25px;
   margin: 40px;
@@ -63,6 +72,17 @@ export default {
   background-color: #f88c19;
   color: white;
   width: 50%;
+}
+#request {
+    background-color: RGB(70,68,68);
+    padding: 1% 2% 2% 2%;
+    margin-top: 5%;
+    width: 50%;
+}
+#backgroundForm {
+display: flex;
+justify-content: center;
+background-color: black;
 }
 #addForm {
   background-color: RGB(70, 68, 68);
