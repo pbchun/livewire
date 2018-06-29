108b1700b9b0fd3d2d1ac84e3f24ba5f703dfc84
Jose Trujillo <jmigueltrujillo@gmail.com>
2018-06-29 14:52:27 -0600

    add components for venue functions

---
+++ gigify/src/components/ApproveEventSection.vue
@@ -0,0 +1,112 @@
+<template>
+    <div>
+        <h2>Submitted Artist Requests</h2>
+        <PendingEvent 
+          :approvedShow="approvedShow"
+        />
+
+        <button
+          class="add-event"
+          @click="toggleApprovedForm()"
+        >
+          Create An Upcoming Show
+        </button>
+
+        <button
+          class="add-event"
+          @click="togglePendingForm()"
+        >
+          List an Open Show Date
+          </button>
+        <button
+          class="add-event"
+          @click="toggleApprovedDates()"
+        >
+          Manage Upcoming Shows
+          </button>          
+        <ManuallyAddEvent
+          v-show="ApprovedFormDisplaying"
+          :API="API"
+        />          
+        <AddAvailableDate
+        v-show="PendingFormDisplaying"
+        :API="API"
+        />    
+        <ManageApprovedDates
+        v-show="ApprovedDatesDisplaying"
+        :API="API"
+        :eventData="eventData"
+        />
+
+    </div>
+</template>
+
+<script>
+import PendingEvent from "@/components/PendingEvent";
+import AddAvailableDate from "@/components/AddAvailableDate";
+import ManuallyAddEvent from "@/components/ManuallyAddEvent";
+import PendingEventVue from "./PendingEvent.vue";
+import ManageApprovedDates from "@/components/ManageApprovedDates"
+
+export default {
+  components: {
+    PendingEvent,
+    AddAvailableDate,
+    ManuallyAddEvent,
+    ManageApprovedDates
+  },
+  data() {
+    return {
+      PendingFormDisplaying: false,
+      ApprovedFormDisplaying: false,
+      ApprovedDatesDisplaying: false,
+      availableDates: [],
+      eventData: [],
+      approvedShow: [],
+      API: {
+        DATE_LISTINGS: "https://whispering-plains-35500.herokuapp.com/events",
+        APPROVED_SHOWS: "https://arcane-chamber-96667.herokuapp.com/events",
+        REQUESTED_SHOWS: "https://pure-taiga-70535.herokuapp.com/events"
+      }
+    };
+  },
+  
+  methods: {
+    togglePendingForm() {
+      this.PendingFormDisplaying = !this.PendingFormDisplaying;
+      this.ApprovedFormDisplaying = false;
+      this.ApprovedDatesDisplaying = false;
+    },
+    toggleApprovedForm() {
+      this.ApprovedFormDisplaying = !this.ApprovedFormDisplaying;
+      this.PendingFormDisplaying = false;
+      this.ApprovedDatesDisplaying = false;
+    },
+    toggleApprovedDates() {
+      this.ApprovedFormDisplaying = !this.ApprovedFormDisplaying;
+      this.PendingFormDisplaying = false;
+      this.ApprovedDatesDisplaying = false;
+    }    
+  },
+  async mounted() {
+    Promise.all([
+      fetch(this.API.DATE_LISTINGS).then(res => res.json()),
+      fetch(this.API.APPROVED_SHOWS).then(res => res.json()),
+      fetch(this.API.REQUESTED_SHOWS).then(res => res.json())
+    ]).then(res => {
+      this.eventData = res;
+    });
+  }
+};
+</script>
+
+<style>
+.add-event {
+  background-color: rgb(70, 68, 68);
+  border-radius: 4px;
+  border: 1px solid rgb(70, 68, 68);
+  color: white;
+  font-size: 25px;
+  margin: 40px;
+}
+</style>
