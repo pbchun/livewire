b3356e4b54c099675e799696b3929a1db7a467ad
Mike Lavin <mikelavin@MacBook-Pro-5.local>
2018-06-29 13:11:30 -0600

    added icons

--- gigify/src/views/About.vue
+++ gigify/src/views/About.vue
@@ -1,35 +1,48 @@
 <template>
+    <div>
+        <h1>MEET THE TEAM</h1>
     <div class  ="profile-container">
         <div class="profile">
             <img src ="../assets/profilePics/reed.png">
             <h2>Reed Black</h2>
             <h5>Software Developer</h5>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui quos nulla porro eum voluptates deleniti quia maiores distinctio laudantium placeat. Voluptate assumenda repellendus, soluta debitis fugit culpa ipsum iusto veritatis!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae voluptas reiciendis ad? Quidem quod fuga possimus cupiditate blanditiis. Iste ut consequuntur odio dolorum, ipsam molestiae aperiam id aspernatur rerum nulla.</p>
+            <img src="../assets/gitIco.png" href="http://www.google.com">
+            <img src="../assets/liIco.png">
         </div>
         <div class="profile">
             <img src="../assets/profilePics/peter.png">
             <h2>Peter Chun</h2>
             <h5>Software Developer</h5>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima dolor esse repudiandae. Corrupti minima placeat dolore adipisci temporibus necessitatibus dignissimos voluptate possimus totam deserunt nam architecto, ea animi dicta explicabo.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae voluptas reiciendis ad? Quidem quod fuga possimus cupiditate blanditiis. Iste ut consequuntur odio dolorum, ipsam molestiae aperiam id aspernatur rerum nulla.</p>
+            <img src="../assets/gitIco.png">
+            <img src="../assets/liIco.png">
         </div>
         <div class="profile">
             <img src="../assets/profilePics/jacob.png">
             <h2>Jacob Hinkston</h2>
             <h5>Software Developer</h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam reiciendis sit dolor sequi pariatur natus itaque ab. Officiis nisi dolorem omnis. Optio totam delectus, alias sequi harum dignissimos id blanditiis?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae voluptas reiciendis ad? Quidem quod fuga possimus cupiditate blanditiis. Iste ut consequuntur odio dolorum, ipsam molestiae aperiam id aspernatur rerum nulla.</p>
+            <img src="../assets/gitIco.png">
+            <img src="../assets/liIco.png">
         </div>
         <div class="profile">
             <img src="../assets/profilePics/mike.png">
             <h2>Mike Lavin</h2>
             <h5>Software Developer</h5>
-            <p>I like my outdoor time a lot. On any day when I am nit making cool stuff on my computer you will find me on my bicycle or on my snowboard. I am a colorado native that like to figure out how things work. I enjoy coming up with cresative solutions to everyday problems</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae voluptas reiciendis ad? Quidem quod fuga possimus cupiditate blanditiis. Iste ut consequuntur odio dolorum, ipsam molestiae aperiam id aspernatur rerum nulla.</p>
+            <img src="../assets/gitIco.png">
+            <img src="../assets/liIco.png">
         </div>
         <div class="profile">
             <img src="../assets/profilePics/jose.png">
             <h2>Jose Trujillo</h2>
             <h5>Software Developer</h5>
-            <p>I like long walks on the beach and going on marathon runs. When I'm never doing those things I enjoy playing board games and music in my free time. Despite the sky high cost of living I currently live in Denver with my girlfriend, Megan, and two dogs, Bukowski and Toki.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae voluptas reiciendis ad? Quidem quod fuga possimus cupiditate blanditiis. Iste ut consequuntur odio dolorum, ipsam molestiae aperiam id aspernatur rerum nulla.</p>
+            <img src="../assets/gitIco.png">
+            <img src="../assets/liIco.png">
         </div>
+    </div>
     </div>
 </template>
 
@@ -38,15 +51,20 @@ export default {};
 </script>
 
 <style scoped>
+h1{
+    margin-top: 100px;
+}
 
 .profile-container{
     display:flex;
     flex-flow:wrap;
+    background-color: black;
+    margin-top: 100px;
 }
 .profile{
     border: solid  black 1px;
     width: 20vw;
-    margin: 20px
+    
 }
 img{
     border-radius: 50%;
