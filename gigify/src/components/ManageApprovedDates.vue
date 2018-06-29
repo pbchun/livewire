3abd5f3849d9ba6b9773002fde3147b015d5ba94
REED BLACK <reedblack@REEDs-MacBook-Air.local>
2018-06-29 14:46:56 -0600

    new section

---
+++ gigify/src/components/ManageApprovedDates.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <ApprovedDate id="flexit" :date="date" v-for="date in eventData[1].events" :key="date.id" />
+    </div>
+</template>
+
+<script>
+import ApprovedDate from "@/components/ApprovedDate";
+
+export default {
+  components: {
+    ApprovedDate
+  },
+  props: ["eventData"]
+};
+</script>
+
+<style>
+</style>
